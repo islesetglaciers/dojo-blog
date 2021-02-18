4bc6dab3e7ea9960aa70229fca8c8f3aa33b55fc
sidd sirius <sidd.benoit@gmail.com>
2021-02-18 10:34:52 -0500

    template refs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,32 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <p ref="p">My name is {{ name }} and I am {{ age }} years old.</p>
+    <button @click="handleClick">Click Me!</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import { ref } from 'vue'
 
 export default {
-  name: 'Home'
+  name: 'Home',
+  setup() {
+    console.log('Setup')
+
+    const p = ref(null)
+
+    let name = 'Mario'
+    let age = 30
+
+    const handleClick = () => {
+      console.log(p, p.value)
+      p.value.classList.add('test')
+      p.value.textContent = 'Hello Ninjas !'
+    }
+
+    return { name, age, handleClick, p }
+  }
 }
 </script>
