cfcbddaae52c6440e40fe0eb25eea30c28b4f213
sidd sirius <sidd.benoit@gmail.com>
2021-02-18 10:55:22 -0500

    refs vs reactive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,58 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <p ref="p">My name is {{ name }} and I am {{ age }} years old.</p>
-    <button @click="handleClick">Click Me!</button>
-    <button @click="age++">Change age</button>
-    <input type="text" v-model="name">
+    <h2>Refs</h2>
+    <!-- <p ref="p">My name is {{ name }} and I am {{ age }} years old.</p> -->
+    <!-- <button @click="handleClick">Click Me!</button> -->
+    <!-- <button @click="age++">Change age</button> -->
+    <!-- <input type="text" v-model="name"> -->
+    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
+    <button @click="updateNinjaOne">Update Ninja One</button>
+    <h2>Reactive</h2>
+    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
+    <button @click="updateNinjaTwo">Update Ninja Two</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 
 export default {
   name: 'Home',
   setup() {
     console.log('Setup')
 
-    const p = ref(null)
+    // const p = ref(null)
 
-    const name = ref('Mario')
-    const age = ref(30)
+    // const name = ref('Mario')
+    // const age = ref(30)
 
-    const handleClick = () => {
+    // const handleClick = () => {
       // console.log(p, p.value)
       // p.value.classList.add('test')
       // p.value.textContent = 'Hello Ninjas !'
-      name.value = 'Luigi'
-      age.value = 28
+      // name.value = 'Luigi'
+      // age.value = 28
+    // }
+
+    // return { name, age, handleClick, p }
+
+    const ninjaOne = ref({ name: 'Mario', age: 30 })
+    const ninjaTwo = reactive({ name: 'Luigi', age: 28 })
+
+    const updateNinjaOne = () => {
+      ninjaOne.value.age = 40
     }
 
-    return { name, age, handleClick, p }
+    // With reactive we don't need to specify we're trying to access the value
+    const updateNinjaTwo = () => {
+      ninjaTwo.age = 35
+    }
+
+    return {
+      ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo
+    }
   }
 }
 </script>
